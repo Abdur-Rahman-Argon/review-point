80a10d09b42c400b63373d2a65cb3b519e9d8e1f
Abdur Rahman Argon <shehab7331@gamil.com>
2022-04-05 23:21:27 +0600

    review three added

--- src/components/HomeReviews/HomeReviews.js
+++ src/components/HomeReviews/HomeReviews.js
@@ -4,12 +4,13 @@ import Review from "../Review/Review";
 
 const HomeReviews = () => {
   const [reviews, setReviews] = useReviews();
+  const review = reviews.slice(0, 3);
 
   return (
     <div>
       <h2 className="text-3xl font-bold p-2 m-5">Customer Review:</h2>
       <div className="md:grid grid-cols-3">
-        {reviews.map((review) => (
+        {review.map((review) => (
           <Review key={review.id} review={review}></Review>
         ))}
       </div>
